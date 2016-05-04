5f520c0774cb889f2ca29c68b151993f09c3068c
Tran Xuan Duc (Trần Xuân Đức) <ductranxuan.29710@gmail.com>
2016-05-04 15:32:32 +0700

    change routes

--- src/maxcmsServiceProvider.php
+++ src/maxcmsServiceProvider.php
@@ -13,17 +13,42 @@ class maxcmsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!$this->app->routesAreCached()) {
-            require __DIR__ . 'routes.php';
-        }
-        $this->loadViewsFrom(base_path('resource/views'), 'maxcms');
+        /**
+         * Add Router from packager load
+         *
+         * @vn: Load trực tiếp các hàm điều khiển (controller) trong gói mở rộng
+         */
+        $this->app->router->group(['namespace' => 'maxsofts\maxcms\Http\Controllers'],
+            function(){
+                require __DIR__.'/Http/routes.php';
+            });
+        /**
+         * Load Auto view from new base_path
+         *
+         * @vn: Load thư mục hiển thị giao diện tại thư mục mới
+         */
+        $this->loadViewsFrom(base_path('MaxCms/views'), 'maxcms');
+        /**
+         * Publishes views
+         *
+         * @vn: Công khai thư mục view mới ( chuyển thư mục views sang thư mục mới để load view)
+         */
         $this->publishes([
-            __DIR__ . '/views' => base_path('resource/views')
+            __DIR__ . '/views' => base_path('MaxCms/views')
         ]);
-
-
+        /**
+         * Publishes themes
+         */
         $this->publishes([
-            __DIR__ . '/migrations' => database_path('migrations')
+            __DIR__ . '/themes' => base_path('public/themes/maxcms')
+        ]);
+        /**
+         * Add Router from packager load
+         *
+         * @vn: Công khai thư mục migrations mới ( chuyển thư mục migrations sang thư mục mới để load database)
+         */
+        $this->publishes([
+            __DIR__ . '/migrations' => database_path('/migrations')
         ], 'migrations');
     }
 
--- src/views/admin.blade.php
+++ src/views/admin.blade.php
@@ -1,8 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tranxuanduc
- * Date: 4/27/16
- * Time: 08:54
- */
 echo "this is test";
